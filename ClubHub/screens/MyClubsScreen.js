344598923cd1179c843cc71a7840ce619628aaef
tazule <jxfiorito@comcast.net>
2020-02-11 09:20:19 -0800

    kind of working (almost)

--- ClubHub/screens/MyClubsScreen.js
+++ ClubHub/screens/MyClubsScreen.js
@@ -7,6 +7,10 @@ import {
   YellowBox
 } from 'react-native';
 
+import * as firebase from 'firebase';
+import 'firebase/firestore';
+import { withFirebase } from 'react-redux-firebase'
+import { compose } from 'redux'
 import { connect } from 'react-redux'
 
 const styles = StyleSheet.create({
@@ -17,19 +21,77 @@ const styles = StyleSheet.create({
   }
 });
 
-export default class MyClubsScreen extends React.Component {
+export class MyClubsScreen extends React.Component {
   constructor(props) {
     super(props);
     YellowBox.ignoreWarnings(['Setting a timer']);
-    console.ignoredYellowBox = ['Setting a timer'];
+    console.ignoredYellowBox = [
+      'Setting a timer'
+    ];
+    this.onClubPress = this.onClubPress.bind(this);
+  }
+  //Runs when one of the clubs is pressed, sets descrId in the store to the ID
+  //of the pressed club so ClubDescrId can take it in
+  onClubPress(item){
+    console.log("Running setDescrId");
+    this.props.setDescrId(item.id);
+    console.log('Store ID state!', store.getState().userClubs.descrId);
+    this.props.navigation.navigate("ClubDescrScreen"); //Get further!
+  }
+  //Style class
+  Separator() {
+    return <View style={styles.separator} />;
   }
   render() {
+    let ClubList;
+    //If the clubs haven't been grabbed yet, display loading text
+    if(!this.props.userClubs){
+      ClubList = <Text style={styles.clubText}>Loading...</Text>;
+    }
+    //For each item in this.props.clubs, create a button with the club name. When it's pressed, pass the club into onClubPress()
+    else {
+      ClubList = <FlatList 
+          data={this.props.userClubs} 
+          renderItem={({item}) => (
+            <View>
+            <TouchableOpacity
+              style={styles.button}
+              onPress={() => this.onClubPress(item)}
+            >
+              <Text style={styles.clubText}>{item.clubName}</Text>
+            </TouchableOpacity>
+            {this.Separator()}
+            </View>
+          )}
+          />;
+    }
     return (
       <View>
         <Text style={styles.mainText}>This is the My Clubs Screen!</Text>
         <Button color="#7700ee" title='Add Clubs (goes to Club Directory)'
         onPress={() => this.props.navigation.navigate('ClubDirectory')}/>
+        {ClubList}
       </View>
     );
   }
 }
+
+//Makes it so the clubs collection is sent to this.props.clubs
+function mapStateToProps(state){
+  return {
+    userClubs: state.firebase.profile.clubs
+  }
+}
+//Makes it so you can use setDescrId(id) by calling this.props.setDescrId(id)
+function mapDispatchToProps(dispatch) {
+  return {
+    setDescrId: id => {
+      dispatch(setDescrId(id))
+    }
+    
+  }
+}
+
+export default compose(
+  withFirebase, 
+  connect(mapStateToProps, mapDispatchToProps))(MyClubsScreen);
