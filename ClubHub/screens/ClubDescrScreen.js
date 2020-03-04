ef0aae13758eb83d6d63009ac87b49bbc2c6e125
theanticrust <oosk2020@gmail.com>
2020-03-03 21:42:34 -0800

    Clip Fix

--- ClubHub/screens/ClubDescrScreen.js
+++ ClubHub/screens/ClubDescrScreen.js
@@ -26,66 +26,49 @@ import { store } from '../redux/store'
 import { styles } from './Styles.js' //Styling for components
 
 class ClubDescrScreen extends React.Component {
-  constructor(props) {
-    super(props);
-    YellowBox.ignoreWarnings(['Setting a timer']);
-    console.ignoredYellowBox = [
-      'Setting a timer'
-    ];
-  }
-  //Separator component, just for styling
-  Separator() {
-    return <View style={styles.separator} />;
-  }
-  //Render the bitty
-  render() {
+	constructor(props) {
+    	super(props);
+    	YellowBox.ignoreWarnings(['Setting a timer']);
+    	console.ignoredYellowBox = [
+		'Setting a timer'
+    	];
+	}
+  	//Separator component, just for styling
+  	Separator() {
+    	return <View style={styles.separator} />;
+  	}
+  	//Render the bitty
+  	render() {
 
-  	var haveClub = false; //for testing add/plus
+  		var haveClub = true; //for testing add/plus
 
-    //If the club info is loading, display loading text. Else, display data
-    if(!this.props.club){
-      return(
-        <View><Text style={styles.clubText}>Loading...</Text></View>
-      );
-    }
-    else {
-
-    	//this is where we need to see if the user has the club in their MyClubs or not
-    	//the buttons will need onPress functions too
-    	if (!haveClub){
+    	//If the club info is loading, display loading text. Else, display data
+    	if(!this.props.club){
+    		return(
+        	<View><Text style={styles.clubText}>Loading...</Text></View>
+      	);
+    	}
+    	else {
       		return (
       			<View>
-      				<Appbar.Header>
-  						<Appbar.Action icon="left" onPress={() => this.props.navigation.navigate("ClubDirectory")} />
+   					<Appbar.Header>
+   						<Appbar.Action icon="left" onPress={() => this.props.navigation.navigate("ClubDirectory")} />
   						<Appbar.Content
-  							title= {this.props.club.clubName}
+  							title= {
+  								(this.props.club.clubName.length > 20) 
+  								? this.props.club.abbrevName
+  								: this.props.club.clubName}
+  							//title={this.props.club.clubName}
   						/>
-  						<Appbar.Action icon="plus"/>
-      				</Appbar.Header>
+  						<Appbar.Action icon={haveClub ? "minus" : "plus"}/>
+   					</Appbar.Header>
         			<Text style={styles.clubText}>Rm. {this.props.club.roomNumber}</Text>
         			<Text style={styles.clubText}>We meet at {this.props.club.when} every {this.props.club.day}</Text>
    					<Text style={styles.clubText}>{this.props.club.shortDesc}</Text>
    				</View>
         	)
-        }
-        else {
-        	return (
-        		<View>
-      				<Appbar.Header>
-  						<Appbar.Action icon="left" onPress={() => this.props.navigation.navigate("ClubDirectory")} />
-  						<Appbar.Content
-  							title= {this.props.club.clubName}
-  						/>
-  						<Appbar.Action icon="minus"/>
-      				</Appbar.Header>
-        			<Text style={styles.clubText}>Rm. {this.props.club.roomNumber}</Text>
-        			<Text style={styles.clubText}>We meet at {this.props.club.when} every {this.props.club.day}</Text>
-   					<Text style={styles.clubText}>{this.props.club.shortDesc}</Text>
-   				</View>
-        	)
-        }
-    }
-  }
+		}
+	}
 }
 
 function mapStateToProps(state) {
